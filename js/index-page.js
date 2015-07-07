2ab93fb1d65cfff86fd16363eb92e9aef1770724
Max Nadolsky <maximusdreddoff@gmail.com>
2015-07-07 15:19:04 +0300

    Surf-commi

---
+++ js/index-page.js
@@ -0,0 +1,40 @@
+﻿
+$(function () {
+    /*
+    - how to call the plugin:
+    $( selector ).cbpFWSlider( [options] );
+    - options:
+    {
+        // default transition speed (ms)
+        speed : 500,
+        // default transition easing
+        
+    }
+    - destroy:
+    $( selector ).cbpFWSlider( 'destroy' );
+    */
+
+    $('#cbp-fwslider').cbpFWSlider({
+        easing: 'ease'
+});
+
+});
+
+$('.carousel[data-type="multi"] .item').each(function () {
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < 2; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
+
+
---
+++ js/user-page.js
@@ -0,0 +1,7 @@
+﻿function blurImage() {
+    var wallpaperWidth = $(".wallpaper-block").css("width"); //taking width of wallpaper
+    $(".js-blurred-image").css("width", wallpaperWidth); //implementing width to blurred image
+}
+$(window).resize(function () {
+    blurImage();
+});
